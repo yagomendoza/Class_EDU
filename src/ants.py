f4e231351dfd8cdea255ac9bd0ecc3c8b2598a84
Edu <edu12due@gmail.com>
2022-03-16 20:57:27 +0100

    ants problem statement

---
+++ src/ants.py
@@ -0,0 +1,11 @@
+"""
+One hundred ants are dropped on a meter stick. Each ant is traveling either to the left or the right with constant speed 1 meter per minute. 
+When two ants meet, they bounce off each other and reverse direction. When an ant reaches an end of the stick, it falls off.
+
+At some point all the ants will have fallen off. 
+The time at which this happens will depend on the initial configuration of the ants.
+
+Question: over ALL possible initial configurations, 
+what is the longest amount of time that you would need to wait to guarantee that the stick has no more ants?
+"""
+
